366018d3606ad3ec684e2b7e1029c4e74fab31e9
kazahayan <127966046+kazahayan@users.noreply.github.com>
2023-03-16 14:18:32 -0400

    Update embed_streamlit.py

--- embed_streamlit.py
+++ embed_streamlit.py
@@ -1,6 +1,9 @@
+import streamlit as st
 import streamlit.components.v1 as components
 
+# Define function to render the embedded Streamlit app
 def render_streamlit_app():
-    components.iframe("https://kazahayan-summary-stats-monthly-release-counts-riulpf.streamlit.app", height=600, scrolling=True)
+    components.iframe("https://kazahayan-summary-stats-monthly-release-counts-riulpf.streamlit.app/", height=600, scrolling=True)
 
+# Call the function to render the embedded Streamlit app
 render_streamlit_app()
