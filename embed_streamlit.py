4aa944f02fe884128d91bbe9c7c49bba43005921
kazahayan <127966046+kazahayan@users.noreply.github.com>
2023-03-16 14:16:24 -0400

    Update embed_streamlit.py

--- embed_streamlit.py
+++ embed_streamlit.py
@@ -1,6 +1,6 @@
 import streamlit.components.v1 as components
 
 def render_streamlit_app():
-    components.iframe("https://kazahayan-summary-stats-monthly-release-counts-riulpf.streamlit.app/", height=600, scrolling=True)
+    components.iframe("https://kazahayan-summary-stats-monthly-release-counts-riulpf.streamlit.app", height=600, scrolling=True)
 
 render_streamlit_app()
